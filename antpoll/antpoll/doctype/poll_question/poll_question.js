e71967d8c7116737e64cad49a8202fe77b9b4573
Avishna <avishnabsofficial@gmail.com>
2025-05-16 13:10:18 +0530

    hide the options for poll users - updated

--- antpoll/antpoll/doctype/poll_question/poll_question.js
+++ antpoll/antpoll/doctype/poll_question/poll_question.js
@@ -3,22 +3,11 @@
 
 frappe.ui.form.on('Poll Question', {
     onload: function(frm) {
-      // Check if the current user has the 'Poll Admin' role
-      frappe.call({
-        method: "frappe.client.get",
-        args: {
-          doctype: "User",
-          name: frappe.session.user
-        },
-        callback: function(r) {
-          if (r.message) {
-            const roles = r.message.roles.map(role => role.role);
-            if (!roles.includes("Poll Admin") && roles.includes("Poll User")) {
-              // Hide the Options child table
-              frm.set_df_property('options', 'hidden', 1);
-            }
-          }
+
+        if (!frappe.user.has_role('Poll Admin') && frappe.user.has_role("Poll User")) {
+            console.log(frappe.user_roles)
+            frm.set_df_property('options', 'hidden', 1);
+
         }
-      });
     }
   });
